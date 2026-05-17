238f49ba1a81208af5623d8dd29c274fa1439cfc
victormba-kalu <mbavictorkalu@gmail.com>
2026-05-17 01:25:32 +0100

    practiced working with ejs

--- server.js
+++ server.js
@@ -24,16 +24,27 @@ app.use(express.static(path.join(__dirname, 'public')));
 /**
   * Routes
   */
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'src/views/home.html'));
+
+// Add this with your other GET routes
+// Set EJS as the templating engine
+app.set('view engine', 'ejs');
+
+// Tell Express where to find your templates
+app.set('views', path.join(__dirname, 'src/views'));
+
+app.get("/", async (req, res) => {
+  const title = "Home";
+  res.render("home", { title });
 });
 
-app.get('/organizations', (req, res) => {
-    res.sendFile(path.join(__dirname, 'src/views/organizations.html'));
+app.get("/organizations", async (req, res) => {
+  const title = "Our Partner Organizations";
+  res.render("organizations", { title });
 });
 
-app.get('/projects', (req, res) => {
-    res.sendFile(path.join(__dirname, 'src/views/projects.html'));
+app.get("/projects", async (req, res) => {
+  const title = "Service Projects";
+  res.render("projects", { title });
 });
 
 app.listen(PORT, () => {
